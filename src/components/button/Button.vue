93fda95e0fc54fce629a07c3bb6119a81e309691
Nurmukhamed Berikbolov <109278086+Nurmukhamed7@users.noreply.github.com>
2023-08-17 17:01:45 +0200

    added style modules

---
+++ src/components/button/Button.vue
@@ -0,0 +1,7 @@
+<template>
+	<button :class="$style.btn">Это кнопка</button>
+</template>
+
+<script></script>
+
+<style module lang="scss" src="./Button.module.scss"></style>
---
+++ src/components/catalog/Catalog.vue
@@ -0,0 +1,7 @@
+<template>
+	<button :class="$style.btn">Это каталог</button>
+</template>
+
+<script setup></script>
+
+<style module lang="scss" src="./Catalog.module.scss"></style>
